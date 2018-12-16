4f1dc546d9843ee8e74962aca44df431cf419045
BZWGolem <kapoplawska@gmail.com>
2018-12-16 17:20:31 +0100

    Last fix. Hope so

--- Server/desks/helpers.py
+++ Server/desks/helpers.py
@@ -119,4 +119,4 @@ def delete_reservation(token, date):
             Reservations.delete().where(Reservations.id_reservation == reservation).execute()
         return {'status': 'Deleted reservation', 'date': date, 'user': user.username}
     else:
-        {'status': 'No reservation', 'date': date, 'user': user.name}
+        {'status': 'No reservation', 'date': date, 'user': user.username}
